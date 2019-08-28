67c4e5092cd65f5ccfa9468626d82a68ec78ca8d
Wunkolo <wunkolo@gmail.com>
2019-08-28 19:53:30 +0000

    Add parallel x2 NEON implementation

--- source/Base2-arm64.hpp
+++ source/Base2-arm64.hpp
@@ -30,7 +30,7 @@ inline void Encode<0>(
 	for( std::size_t i = 0; i < Length; ++i )
 	{
 		// Broadcast byte across 8 byte lanes
-		uint8x8_t Word = vdup_n_u8(Input[i]);
+		uint8x8_t Word = vld1_dup_u8(Input + i);
 		// Mask unique bits in each byte
 		Word = vand_u8(Word, vreinterpret_u8_u64(UniqueBit));
 		// Shift unique bit to upper bit of each 8-bit lane
@@ -47,56 +47,39 @@ inline void Encode<0>(
 }
 
 // Two at a time
-#if defined(__SSE2__)
 template<>
 inline void Encode<1>(
 	const std::uint8_t Input[], std::uint64_t Output[], std::size_t Length
 )
 {
-	constexpr std::uint64_t LSB8          = 0x0101010101010101UL;
-	constexpr std::uint64_t UniqueBit     = 0x0102040810204080UL;
-	constexpr std::uint64_t CarryShift    = 0x7F7E7C7870604000UL;
-
+	// Least significant bit in an 8-bit integer
+	const uint8x16_t MSB8 = vdupq_n_u8(0b10000000);
+	// Constant bits for ascii '0' and '1'
+	const uint8x16_t BinAsciiBasis = vdupq_n_u8('0');
+	const uint64x2_t UniqueBit  = vdupq_n_u64(0x0102040810204080UL);
+	const uint64x2_t CarryShift = vdupq_n_u64(0x7F7E7C7870604000UL);
 	std::size_t i = 0;
 	for( ; i < Length; i += 2 )
 	{
-	#if defined(__SSSE3__)
-		__m128i Result = _mm_set1_epi16(
-			*reinterpret_cast<const std::uint16_t*>(&Input[i])
-		);
-		// Upper and lower 64-bits get filled with bytes
-		Result = _mm_shuffle_epi8(Result, _mm_set_epi64x(LSB8 * 1, LSB8 * 0));
-	#else
-		__m128i Result = _mm_set_epi64x(
-			LSB8 * static_cast<std::uint64_t>(Input[i + 1]),
-			LSB8 * static_cast<std::uint64_t>(Input[i + 0])
-		);
-	#endif
-		// Mask Unique bits per byte
-		Result = _mm_and_si128(Result, _mm_set1_epi64x(UniqueBit));
-		// Use the carry-bit to slide it to the far left
-		Result = _mm_add_epi64(Result, _mm_set1_epi64x(CarryShift));
-	#if defined(__SSE4_1__)
-		// Pick between ascii '0' and '1', using the upper bit in each byte
-		Result = _mm_blendv_epi8(
-			_mm_set1_epi8('0'), _mm_set1_epi8('1'), Result
-		);
-	#else
-		constexpr std::uint64_t BinAsciiBasis = LSB8 * '0';
-		constexpr std::uint64_t MSB8          = LSB8 << 7u;
-		// Mask this last bit
-		Result = _mm_and_si128(Result, _mm_set1_epi64x(MSB8));
-		// Shift it to the low bit of each byte
-		Result = _mm_srli_epi64(Result, 7);
-		// Convert it to ascii `0` and `1`
-		Result = _mm_or_si128(Result, _mm_set1_epi64x(BinAsciiBasis));
-	#endif
-		_mm_store_si128(reinterpret_cast<__m128i*>(&Output[i]), Result);
+		const uint8x8x2_t Input2 = vld2_dup_u8(Input + i);
+		// Broadcast byte across 8 byte lanes
+		uint8x16_t Word2 = vcombine_u8(Input2.val[0], Input2.val[1]);
+		// Mask unique bits in each byte
+		Word2 = vandq_u8(Word2, vreinterpretq_u8_u64(UniqueBit));
+		// Shift unique bit to upper bit of each 8-bit lane
+		Word2 = vaddq_u8(Word2, vreinterpretq_u8_u64(CarryShift));
+		// Mask upper bit
+		Word2 = vandq_u8(Word2, MSB8);
+		// Shift it back down
+		Word2 = vshrq_n_u8(Word2, 7u);
+		// binary-or with '0'
+		Word2 = vorrq_u8(Word2, BinAsciiBasis);
+		// store
+		vst1q_u64(Output + i, vreinterpretq_u64_u8(Word2));
 	}
 
 	Encode<0>(Input + i * 2, Output + i * 2, Length % 2);
 }
-#endif
 
 }
 
